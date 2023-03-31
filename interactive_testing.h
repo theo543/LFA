eba18c17160927abc4f034f583b5e3eab6b4534a
Theodor Negrescu <theodor.negrescu@gmail.com>
2023-03-31 15:05:15 +0300

    Extract interactive testing to new file

---
+++ interactive_testing.h
@@ -0,0 +1,10 @@
+//
+// Created by THEO on 31.03.2023.
+//
+
+#ifndef LFA_INTERACTIVE_TESTING_H
+#define LFA_INTERACTIVE_TESTING_H
+
+void interactive_testing_mode();
+
+#endif //LFA_INTERACTIVE_TESTING_H
